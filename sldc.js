bd9324e862f4c7325c1e23ed4692adee1c06a322
Zack Rogers <zack.rogers.t@gmail.com>
2022-05-27 10:12:30 -0600

    reordered process steps and created variables with definitions.

--- sldc.js
+++ sldc.js
@@ -5,15 +5,23 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
+// Planning
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
+
+
+let planning = "Planning: Plan what the project will do and all the needs it must fulfill"
+let analysisOfRequirements = "Analysis of Requirements: Analyze the the restraints and requirements of building the project"
+let design = "Design: Design the different interfaces and functions of a program to meet the needs of the project"
+let implementation = "Implementation: Implement the design and functionality of a program into one useable system"
+let testingAndIntegration = "Testing & Integration: Test the program for any bugs or issues then integrate the system into the hands of the users."
+let maintain = "Maintain: Maintain the program by repeating the steps to resolve any bugs, changes, or updates."
