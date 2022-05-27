40f135a7c922e6ae41729fa226bd93b31f5814b5
Zack Rogers <zack.rogers.t@gmail.com>
2022-05-27 12:48:49 -0600

    added additional constraints for password

--- password.js
+++ password.js
@@ -1,6 +1,8 @@
 
+const verified = ["10+ characters", "1+ UPPER-CASE letter", "1+ lower-case letter", "1+ of any of the following characters: ! @ # $ % ^ & *"]
 
-console.log("Welcome to Zackaroni's Password Validation Tool.");
+console.log("Welcome to Zackaroni's Password Validation Tool.", "To validate your password it must contain", verified);
+
 
 const readline = require('readline');
 const reader = readline.createInterface({
@@ -12,12 +14,16 @@ reader.question("Please enter a password to validate:", function (input) {
     const tokens = input.split(' ');
     const password = tokens[0];
 
+
+    const lowerCaseLetters = (/[a-z]/g);
+
+
     let passMinLength = 10;
 
-    if (password.length >= passMinLength) {
-        console.log("Success: password meets or exceeds", passMinLength, "characters and has been valiated.");
+    if (password.length >= passMinLength && /[a-z]/.test(password) && /[A-Z]/.test(password) && password.includes('!', '@', '#', '$', '%', '^', '&', '*')) {
+        console.log("Success: password valiated.");
     } else {
-        console.log("Failed: password does not meet or exceed", passMinLength, "characters. Recommended to update password length.");
+        console.log("Failed: does not meet minimum requirements. Please review requirements.");
     }
 
     reader.close();
