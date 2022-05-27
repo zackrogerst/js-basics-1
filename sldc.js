c6800a66c22149ea3ae8f5d1704d2e637cc7f64b
Zack Rogers <zack.rogers.t@gmail.com>
2022-05-27 09:55:11 -0600

    added to repository

---
+++ sldc.js
@@ -0,0 +1,19 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Analysis of Requirements
+// Design
+// Implementation
+// Maintain
+// Planning
+// Testing & Integration
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+*/
