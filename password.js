390befad88c0ce34016f033b0c8635320e09a3f9
Zack Rogers <zack.rogers.t@gmail.com>
2022-05-27 13:02:47 -0600

    added addiitional constraints to password validator

--- password.js
+++ password.js
@@ -1,5 +1,5 @@
 
-const verified = ["10+ characters", "1+ UPPER-CASE letter", "1+ lower-case letter", "1+ of any of the following characters: ! @ # $ % ^ & *"]
+const verified = ["10+ characters", "1+ UPPER-CASE letter", "1+ lower-case letter", "1+ of any of the following characters: ! @ # $ % ^ & *", "1+ number"]
 
 console.log("Welcome to Zackaroni's Password Validation Tool.", "To validate your password it must contain", verified);
 
@@ -19,8 +19,34 @@ reader.question("Please enter a password to validate:", function (input) {
 
 
     let passMinLength = 10;
+    let passwordCharacter = false;
+    let charArr = ['!', '@', '#', '$', '%', '^', '&', '*']
 
-    if (password.length >= passMinLength && /[a-z]/.test(password) && /[A-Z]/.test(password) && password.includes('!', '@', '#', '$', '%', '^', '&', '*')) {
+    for (i = 0; i <= password.length; i++){
+        if(password.includes("!")){
+            passwordCharacter = true;
+        } else if (password.includes("@")){
+            passwordCharacter = true;
+        } else if (password.includes("#")){
+            passwordCharacter = true;
+        } else if (password.includes("$")){
+            passwordCharacter = true;
+        } else if (password.includes("%")){
+            passwordCharacter = true;
+        } else if (password.includes("^")){
+            passwordCharacter = true;
+        } else if (password.includes("&")){
+            passwordCharacter = true;
+        } else if (password.includes("*")){
+            passwordCharacter = true;
+        } else {
+            passwordCharacter = false;
+            console.log(passwordCharacter)
+        }
+    }
+
+
+    if (password.length >= passMinLength && /[a-z]/.test(password) && /[A-Z]/.test(password) && /[0-9]/.test(password) && passwordCharacter === true) {
         console.log("Success: password valiated.");
     } else {
         console.log("Failed: does not meet minimum requirements. Please review requirements.");
